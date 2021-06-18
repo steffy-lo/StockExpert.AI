28545d3ce748213f4d3689d8d1988a0c745e86dd
Steffy Lo <steffy@bambu.co>
2021-06-18 15:21:08 +0800

    no helmet

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-app.use(helmet());
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.static(__dirname + "/build"));
